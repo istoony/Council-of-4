eb8955f7d539a5494bb42dd6a866e62fae207cd8
istoony <antonio.gianola@mail.polimi.it>
2016-06-13 11:23:20 +0200

    Speriamo V2

---
+++ src/main/java/it/polimi/ingsw/PS19/model/bonus/GetCityBonus.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.ps19.model.bonus;
+import it.polimi.ingsw.ps19.model.Player;
+
+public class GetCityBonus implements Bonus {
+
+	/**
+	 * 
+	 */
+	// not done
+	private static final long serialVersionUID = 6314285377196367956L;
+	
+	private int howmany;
+
+	public GetCityBonus(int parameter) {
+		howmany = parameter;
+	}
+
+	@Override
+	public void giveBonus(Player p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
