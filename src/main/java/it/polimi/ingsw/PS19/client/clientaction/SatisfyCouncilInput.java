026f149223943a7fc2f8fae426c8d80cbd8daa4a
Andrea Milanta <andrea.milanta@gmail.com>
2016-06-17 02:08:34 +0200

    Card chooser algorithm basic implementation

--- src/main/java/it/polimi/ingsw/PS19/client/clientaction/SatisfyCouncilInput.java
+++ src/main/java/it/polimi/ingsw/PS19/client/clientaction/SatisfyCouncilInput.java
@@ -16,7 +16,7 @@ import it.polimi.ingsw.PS19.model.parameter.Costants;
  * Abstract class to be implemented by those inputs which require to satisfy a council
  */
 public abstract class SatisfyCouncilInput extends ClientAction 
-{
+{	
 	protected List<RegionType> getAvailableRegions()
 	{
 		List<RegionType> availableRegions = new ArrayList<>();
@@ -64,16 +64,16 @@ public abstract class SatisfyCouncilInput extends ClientAction
 	
 	private int getJollyNumber(List<PoliticsCard> cards)
 	{
-		return getJollies(cards).size();
-	}
-	
-	private List<PoliticsCard> getJollies(List<PoliticsCard> cards)
-	{
-		List<PoliticsCard> jollies = new ArrayList<>();
+		int jolliesNumber = 0;
 		for(PoliticsCard card : cards)
 			if(card.getColor().equals(Color.decode(Costants.JOKERCOLOR)))
-				jollies.add(card);
-		return jollies;
+				jolliesNumber++;	
+		return jolliesNumber;
+	}
+	
+	private PoliticsCard newJolly()
+	{
+		return new PoliticsCard(Color.decode(Costants.JOKERCOLOR));
 	}
 
 	protected List<PoliticsCard> getAvailablePolitics(List<Color> balcony, List<PoliticsCard> cards)
@@ -90,11 +90,11 @@ public abstract class SatisfyCouncilInput extends ClientAction
 				{
 					availableCards.add(cards.get(i));
 					cards.remove(i);
-					break;
 				}
 			}
 		}
-		availableCards.addAll(getJollies(cards));
+		if(getJollyNumber(cards) >0)
+			availableCards.add(newJolly());
 		return availableCards;
 	}
 	
@@ -136,4 +136,303 @@ public abstract class SatisfyCouncilInput extends ClientAction
 		else 
 			return 4;
 	}
+	
+	/**
+	 * returns only decks which are not equivalent between each others
+	 * @param decks: general list of decks
+	 * @return
+	 */
+	private List<DeckId> getSignificantDecks(List<DeckId> decks)
+	{
+		if(decks.isEmpty())
+			return decks;
+		List<DeckId> significantDecks = new ArrayList<>();
+		for(DeckId deckComparing : decks)
+		{
+			significantDecks.add(deckComparing);
+			for(DeckId deckCompared : decks)
+				if(deckComparing.equivalent(deckCompared))
+					decks.remove(deckCompared);
+		}
+		return significantDecks;
+	}
+	
+	/**
+	 * Returns only the decks that satisfy the reference decks;
+	 * @param decks: decks to be validated
+	 * @param referenceDeck: Balcony deck
+	 * @return
+	 */
+	private List<DeckId> getValidDecks(List<DeckId> decks, DeckId referenceDeck)
+	{
+		List<DeckId> validDecks = new ArrayList<>();
+		for(DeckId deck : decks)
+			if(deck.equivalentOrContained(referenceDeck))
+				validDecks.add(deck);
+		return validDecks;
+	}
+
+	/*
+	 * TODO Metodo che fa tutto
+	 * Deve capire quante carte minimo
+	 * trova le combinazioni con quel numero di carte e more
+	 * prende le combinazioni valide
+	 * Trova i diversi colori
+	 * ne fa scegliere uno
+	 * si salva la carta
+	 * ricomincia da capo considerando solo le soluzioni contenenti quella carta
+	 * valuta se può smettere
+	 * 
+	 * Per i jokers da pensare una minima
+	 */
+	
+	
+	private List<DeckId> getContainingDecks(List<DeckId> decks, PoliticsCard polCard)
+	{
+		if(decks.isEmpty())
+			return decks;
+		CardId card = new CardId(polCard, 0);
+		List<DeckId> containingDecks = new ArrayList<>();
+		for(DeckId deck : decks)
+			if(deck.contains(card))
+				containingDecks.add(deck);
+		return containingDecks;
+	}
+	
+	private List<Color> getDifferentColors(List<DeckId> decks)
+	{
+		List<CardId> cards = new ArrayList<>();
+		if(decks.isEmpty())
+			return null;
+		List<CardId> differentCards = new ArrayList<>();
+		for(DeckId deck : decks)
+			cards.addAll(deck.getDeckClone());
+		for(CardId card : cards)
+		{
+			differentCards.add(card);
+			cards.remove(card);
+			for(CardId listCard : cards)
+				if(card.equivalent(listCard))
+					cards.remove(listCard);
+		}
+		List<Color> differentColors = new ArrayList<>();
+		for(CardId card : differentCards)
+			differentColors.add(card.getColor());
+		return differentColors;
+	}
+	
+	private class CardId
+	{
+		private Color color;
+		private int id;
+		public CardId(PoliticsCard card, int identifier)
+		{
+			color = card.getColor();
+			id = identifier;
+		}
+		
+		public CardId(Color c, int identifier)
+		{
+			color = c;
+			id = identifier;
+		}
+		
+		public Color getColor()
+		{
+			return color;
+		}
+		
+		public int getId()
+		{
+			return id;
+		}
+		
+		public boolean equals(CardId cardId)
+		{
+			if(id == cardId.getId() && color.equals(cardId.getColor()))
+				return true;
+			return false;
+		}
+		
+		public boolean equivalent(CardId cardId)
+		{
+			if(color.equals(cardId.getColor()))
+				return true;
+			return false;
+		}
+	}
+	
+	private class DeckId
+	{
+		List<CardId> cardsId;
+		private final CardId jolly = new CardId(Color.decode(Costants.JOKERCOLOR), 0);
+		
+		/**
+		 * Constructor
+		 * @param cards: list of cards/cardiID or colors to be converted in deck
+		 */
+		@SuppressWarnings("unchecked")
+		public DeckId(List<?> cards)
+		{
+			if(cards.isEmpty())
+				return;
+			if(cards.get(0) instanceof PoliticsCard)
+			{
+				cardsId = new ArrayList<>();
+				for(int i = 0; i < cards.size(); i++)
+					cardsId.add(new CardId((PoliticsCard)cards.get(i), i));
+				return;
+			}
+			if(cards.get(0) instanceof CardId)
+			{
+				cardsId = new ArrayList<>();
+				cardsId.addAll((ArrayList<CardId>)cards);
+				return;
+			}
+			if(cards.get(0) instanceof Color)
+			{
+				cardsId = new ArrayList<>();
+				for(int i = 0; i < cards.size(); i++)
+					cardsId.add(new CardId((Color)cards.get(i), i));
+				return;
+			}
+		}
+		
+		/**
+		 * returns a reference to the list of cards in the deck
+		 * @return
+		 */
+		public List<CardId> getDeck()
+		{
+			return cardsId;
+		}
+		
+		/**
+		 * returns a clone of the contained deck
+		 * @return
+		 */
+		public List<CardId> getDeckClone()
+		{
+			List<CardId> clone = new ArrayList<>();
+			clone.addAll(cardsId);
+			return clone;
+		}
+		
+		/**
+		 * Returns whether the two deck are equivalent
+		 * @param deck: comparing deck
+		 * @return
+		 */
+		public boolean equivalent(DeckId deck)
+		{
+			if(deck.getDeckClone().size() != cardsId.size())
+				return false;
+			return equivalentOrContained(deck);
+		}
+		
+		/**
+		 * Returns whether the deck calling is equivalent or contained in the parameter
+		 * @param deck: Containing deck;
+		 * @return
+		 */
+		public boolean equivalentOrContained(DeckId deck)
+		{
+			List<CardId> secondDeck = deck.getDeckClone();
+			List<CardId> firstDeck = this.getDeckClone();
+			for(CardId cardOne : firstDeck)
+				for(CardId cardTwo : secondDeck)
+					if(cardOne.equivalent(cardTwo))
+					{
+						firstDeck.remove(cardOne);
+						secondDeck.remove(cardTwo);
+						break;
+					}
+			if(firstDeck.isEmpty())
+				return true;
+			return false;
+		}
+		
+		/**
+		 * Returns whether the deck calling is equivalent or contains the parameter
+		 * @param deck: Contained deck
+		 * @return
+		 */
+		public boolean equivalentOrContaining(DeckId deck)
+		{
+			return deck.equivalentOrContained(this);
+		}
+		
+		public boolean contains(CardId card)
+		{
+			for(CardId deckCard : cardsId)
+				if(card.equivalent(deckCard))
+					return true;
+			return false;
+		}
+		
+		/**
+		 * returns a list of decks with all the possible combinations of n cards of the deck;
+		 * @param n: number of cards in combination
+		 * @return 
+		 */
+		public List<DeckId> getCombination(int n)
+		{
+			List<DeckId> combinations = new ArrayList<>();
+			if(n >= cardsId.size())
+			{
+				combinations.add(this);
+				return combinations;
+			}
+			int[] s = new int[n];
+		    for (int i = 0; (s[i] = i) < n - 1; i++);  // first creates the index sequence: 0, 1, 2, ...
+		    combinations.add(getSubset(this, s));
+		    while(true)
+		    {
+		        int i;
+		        // find position of item that can be incremented
+		        for (i = n - 1; i >= 0 && s[i] == cardsId.size() - n + i; i--); 
+		        if (i < 0)
+		            break;
+		        else 
+		        {
+		            s[i]++;                    // increment this item
+		            for (++i; i < n; i++)	   // fill up remaining items
+		                s[i] = s[i - 1] + 1; 
+		            combinations.add(getSubset(this, s));
+		        }
+			}
+			return combinations;
+		}
+		
+		public DeckId subtractCard(CardId card)
+		{
+			if(!contains(card))
+				return this;
+			List<CardId> deck = getDeckClone();
+			for(CardId deckCard : deck)
+				if(deckCard.equivalent(card))
+				{
+					if(deck.remove(deckCard));
+					break;
+				}
+			return new DeckId(deck);
+		}
+		
+		public DeckId getCleanDeck()
+		{
+			List<CardId> cleanDeck = new ArrayList<>();
+			for(CardId card : cardsId)
+				if(!card.equivalent(jolly))
+					cleanDeck.add(card);
+			return new DeckId(cleanDeck);
+		}
+
+		private DeckId getSubset(DeckId input, int[] subset) {
+			List<CardId> result = new ArrayList<>();
+		    for (int i = 0; i < subset.length; i++) 
+		        result.add(i, input.getDeck().get(subset[i]));
+		    return new DeckId(result);
+		}
+	}
 }
+
