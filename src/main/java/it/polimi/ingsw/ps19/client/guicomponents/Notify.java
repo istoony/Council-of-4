9b7738da74ff6b171c67ce512ee3ff05a80a61ad
Andrea <Andrea@Alias.mshome.net>
2016-06-22 14:55:19 +0200

    hello world

--- src/main/java/it/polimi/ingsw/ps19/client/guicomponents/Notify.java
+++ src/main/java/it/polimi/ingsw/ps19/client/guicomponents/Notify.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.ps19.client.guicomponents;
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,25 +15,38 @@ public class Notify extends JFrame implements Runnable{
 	 */
 	private static final long serialVersionUID = 4932512229185402449L;
 	private JPanel main;
-	private JLabel text;
+	private List<JLabel> text;
 	
 	public Notify(String s){
-		super();
+		super("Messages");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		main = new JPanel();
-		text = new JLabel(s);
+		text = new ArrayList<>();
+		text.add(new JLabel(s));
+		setLayout(new GridLayout(text.size(), 1));
 		setVisible(false);
+	}
+	
+	public void addMessage(String s){
+		text.add(new JLabel(s));
+		setStuff();
 	}
 		
 	@Override
 	public void run() {
-		setBounds(200, 200, 300, 400);
-		text.setVisible(true);
+		setBounds(300, 300, 300, 400);
 		main.setVisible(true);
-		main.add(text);
 		add(main);
 		pack();
 		setVisible(true);
 		setAutoRequestFocus(true);
+	}
+	
+	public void setStuff(){
+		setLayout(new GridLayout(text.size(), 1));
+		for(JLabel j: text){
+			j.setVisible(true);
+			main.add(j);
+		}
 	}
 }
