c28e23392b811d496c1c8a25c0ffe546774021a6
Andrea <Andrea@Alias.mshome.net>
2016-06-22 15:22:49 +0200

    i+1

--- src/main/java/it/polimi/ingsw/ps19/client/guicomponents/MainFrame.java
+++ src/main/java/it/polimi/ingsw/ps19/client/guicomponents/MainFrame.java
@@ -26,7 +26,8 @@ public class MainFrame extends JFrame {
 		this.model=model;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(true);
-		setBounds(100, 60, MWIDTH, MHEIGHT);
+		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//setBounds(100, 60, MWIDTH, MHEIGHT);
 		setMinimumSize(this.getSize());
 		getContentPane().setLayout(new BorderLayout());
 	}
